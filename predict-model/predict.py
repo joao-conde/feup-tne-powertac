ded86f0d3c5acfc8641030317b1af3f58182de47
joao-conde <up201503256@fe.up.pt>
2019-05-15 14:04:52 +0100

    Coefficients given to each attribute

--- predict-model/predict.py
+++ predict-model/predict.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt  
 from sklearn import datasets, linear_model, model_selection, metrics
 
 
 def main():
     data = pd.read_csv('data.csv')
 
-    x = data[["Timeslot", "NoBrokers", "NoCustomers", "Production", "Consumption24hAgo", "Production24Ago", "TempForecast", "WindSpeedForecast", "WindDirectionForecast", "CloudsForecast", "ClearedQuantity24h", "Imbalance", "Consumption-ProductionIn24h"]].values
+    attr_cols = ["NoBrokers", "NoCustomers", "Consumption24hAgo", "Production24Ago", "TempForecast", "WindSpeedForecast", "WindDirectionForecast", "CloudsForecast", "ClearedQuantity24h", "Consumption-ProductionIn24h"]
+    x = data[attr_cols].values
     y = data['Consumption'].values
 
     x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
@@ -22,6 +22,9 @@ def main():
     print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
     print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
     print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+  
+    coeff_df = pd.DataFrame(regressor.coef_, attr_cols, columns=['Coefficient'])  
+    print(coeff_df)
 
 if __name__ == "__main__":
     main()
