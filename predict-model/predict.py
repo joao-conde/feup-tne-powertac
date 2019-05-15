59d0d7a93bba305c8d1709e4be23f438cc72c491
Renato Campos <renatoineeve@gmail.com>
2019-05-15 11:42:35 +0100

    Dados

--- predict-model/predict.py
+++ predict-model/predict.py
@@ -1,18 +1,18 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt  
-from sklearn import datasets, linear_model, model_selection, metrics
+from sklearn import datasets, linear_model, model_selection, metrics, neural_network
 
 
 def main():
     data = pd.read_csv('data.csv')
 
-    x = data[["Timeslot", "NoBrokers", "NoCustomers", "Production", "Consumption24hAgo", "Production24Ago", "TempForecast", "WindSpeedForecast", "WindDirectionForecast", "CloudsForecast", "ClearedQuantity24h", "Imbalance", "Consumption-ProductionIn24h"]].values
-    y = data['Consumption'].values
+    x = data[["Timeslot", "NoBrokers", "NoCustomers", "Consumption", "Production", "Consumption24hAgo", "Production24Ago", "TempForecast", "WindSpeedForecast", "WindDirectionForecast", "CloudsForecast", "ClearedQuantity", "ClearedQuantity24h", "Imbalance"]].values
+    y = data['Imbalance24hSign'].values
 
     x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
 
-    regressor = linear_model.LinearRegression()  
+    regressor = neural_network.MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
     regressor.fit(x_train, y_train)
 
     y_pred = regressor.predict(x_test)
