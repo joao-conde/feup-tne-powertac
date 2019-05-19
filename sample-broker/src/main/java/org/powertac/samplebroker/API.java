99ba6509a3c0008f29b7c2ed0186f47e1e71b1cc
Renato Campos <renatoineeve@gmail.com>
2019-05-19 22:09:37 +0100

    Import http client

--- sample-broker/src/main/java/org/powertac/samplebroker/API.java
+++ sample-broker/src/main/java/org/powertac/samplebroker/API.java
@@ -1,15 +1,12 @@
 package org.powertac.samplebroker;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,16 +21,14 @@ public class API {
     }
 
     public static String getPrediction(String[] data) throws Exception {
-
-        HttpClient httpClient = new DefaultHttpClient();
+        CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost("http://localhost:5000/predict/");
         httpPost.setHeader("Content-type", "application/json");
         try {
             StringEntity stringEntity = new StringEntity(data);
             httpPost.getRequestLine();
             httpPost.setEntity(stringEntity);
-    
-            httpClient.execute(httpPost);
+            CloseableHttpResponse response2 = httpClient.execute(httpPost);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
